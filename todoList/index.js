cfc04e5525ac23c90e9c09582c557487e9236cc4
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2024-06-05 21:19:19 -0300

    atividade do dia 04/06/2024

--- todoList/index.js
+++ todoList/index.js
@@ -1,6 +1,9 @@
 let listaSalva = localStorage.getItem("@listagem_task");
 let tarefas = listaSalva!==null && JSON.parse(listaSalva) || []; 
 const main = document.querySelector("#main");
+let editId = null;
+
+list();
 
 function saveTask(e){
     e.preventDefault();
@@ -9,9 +12,6 @@ function saveTask(e){
     const form = e.target;
 
     const formData = new FormData(form);
-
-
-    alert("chamou")
   
     if(formData.get('task') === " " || formData.get('description') === " "){
      alert("Valores vazios");
@@ -25,8 +25,8 @@ function saveTask(e){
    
       tarefas.push(task);
   
-      // list();
      saveData();
+     location.reload();
     }
   
   }
@@ -80,7 +80,23 @@ function saveTask(e){
 
       const p = document.createElement("p");
       p.innerText = item.description;
+      p.setAttribute("style", "margin-top:20px;")
   
+      const edit = document.createElement("button");
+      edit.append("✏️");
+      edit.setAttribute("class", "edit_button");
+      edit.setAttribute("title", "Editar Tarefa");
+      edit.setAttribute("onclick", `openEditDialog(${tarefas.indexOf(item)})`);
+
+
+      const delet = document.createElement("button");
+      delet.append("❌");
+      delet.setAttribute("title", "Deletar Tarefa");
+      delet.setAttribute("class", "delete_button");
+      delet.setAttribute("onclick",`deletarTarefa(${tarefas.indexOf(item)})` );
+
+      field.appendChild(delet)
+      field.appendChild(edit)
       field.appendChild(h2);
       field.appendChild(p);
 
@@ -89,4 +105,56 @@ function saveTask(e){
     })
   }
 
-  list()
+
+
+  function openEditDialog(taskId) {
+    console.log(taskId)
+    editId = taskId;
+  
+   // const selectedTaskId = tarefas.findIndex((task) => task.id === taskId)
+    const task = tarefas[taskId]
+  
+    const dialog = document.querySelector('dialog')
+  
+    const editTitle = document.querySelector('#editTaskForm #title')
+    const editDescription = document.querySelector('#editTaskForm #description')
+  console.log(task)
+    editTitle.value = task.name
+    editDescription.value = task.description
+  
+    dialog.showModal()
+
+  }
+  
+  function closeDialog() {
+    const dialog = document.querySelector('dialog')
+    dialog.close()
+  }
+
+
+ function deletarTarefa(pos){
+
+  tarefas.splice(pos, 1);
+  saveData();  
+  location.reload();
+ }
+
+ function editTask(event){
+  event.preventDefault();
+
+  const editTitle = document.querySelector('#editTaskForm #title')
+  const editDescription = document.querySelector('#editTaskForm #description')
+
+
+ const taskEdit ={
+  name : editTitle.value,
+  description : editDescription.value
+ } 
+
+
+ 
+ tarefas[editId] = taskEdit;
+
+ saveData();
+ closeDialog();
+ }
